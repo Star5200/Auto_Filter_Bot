33954d0cc571419d64692709b4054acf92af9b93
Star5200 <stargaming19062002@gmail.com>
2026-09-07 19:07:04 +0530

    Fix formatting of InlineKeyboardButton in pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -1248,7 +1248,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton('‼️ ᴅɪꜱᴄʟᴀɪᴍᴇʀ ‼️', callback_data='disclaimer'),
-            InlineKeyboardButton ('🪔 sᴏᴜʀᴄᴇ', callback_data='source'),
+            
         ],[
             InlineKeyboardButton('ᴅᴏɴᴀᴛɪᴏɴ 💰', callback_data='donation'),
         ],[
