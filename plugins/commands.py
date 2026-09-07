83a23f8fc9122431990fa32e638f3e1f0f37af8c
Star5200 <stargaming19062002@gmail.com>
2026-09-07 21:25:07 +0530

    Add movie group button to command options

--- plugins/commands.py
+++ plugins/commands.py
@@ -109,7 +109,8 @@ async def start(client, message):
         if len(message.command) != 2:
             buttons = [[
                         InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
-                    ],[
+                    ],[ InlineKeyboardButton('🎬 MOVIE GROUP 🎬', url='https://t.me/+DNA3Uc3wisU5ODA1')
+                      ],[
                         InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
                         InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
                     ],[
