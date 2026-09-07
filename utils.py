ea73a41774e7bc12f9388b4850fd5f9fe3a3d125
Star5200 <stargaming19062002@gmail.com>
2026-09-07 22:24:01 +0530

    Remove powered by line from captions

--- utils.py
+++ utils.py
@@ -1067,7 +1067,7 @@ async def get_cap(settings, remaining_seconds, files, query, total_results, sear
                             f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n"
                             f"⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n\n"
                             f"📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {query.from_user.mention}\n"
-                            f"⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ :⚡ {query.message.chat.title or temp.B_LINK or 'ᴅʀᴇᴀᴍxʙᴏᴛᴢ'}\n</b>"
+                            
                         )
                     else:
                         cap = (
@@ -1075,7 +1075,7 @@ async def get_cap(settings, remaining_seconds, files, query, total_results, sear
                             f"🧱 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n"
                             f"⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n\n"
                             f"📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {query.from_user.mention}\n"
-                            f"⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ :⚡ {query.message.chat.title or temp.B_LINK or 'ᴅʀᴇᴀᴍxʙᴏᴛᴢ'}\n</b>"
+                            
                         )
                     cap += "\n\n<u>Your Requested Files Are Here</u> \n\n</b>"
                     for idx, file in enumerate(files, start=offset + 1):
@@ -1093,7 +1093,7 @@ async def get_cap(settings, remaining_seconds, files, query, total_results, sear
                 cap = (
                     f"<b>🏷 ᴛɪᴛʟᴇ : <code>{search}</code>\n"
                     f"⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n\n"
-                    f"⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {query.message.chat.title or temp.B_LINK or 'ᴅʀᴇᴀᴍxʙᴏᴛᴢ'}\n</b>"
+                    
                 )
             else:
                 cap = (
@@ -1101,7 +1101,7 @@ async def get_cap(settings, remaining_seconds, files, query, total_results, sear
                     f"🧱 ᴛᴏᴛᴀʟ ꜰɪʟᴇꜱ : <code>{total_results}</code>\n"
                     f"⏰ ʀᴇsᴜʟᴛ ɪɴ : <code>{remaining_seconds} Sᴇᴄᴏɴᴅs</code>\n\n"
                     f"📝 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ : {query.from_user.mention}\n"
-                    f"⚜️ ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⚡ {query.message.chat.title or temp.B_LINK or 'ᴅʀᴇᴀᴍxʙᴏᴛᴢ'}\n</b>"
+                    
                 )
 
             cap += "\n\n<u>Your Requested Files Are Here</u>\n\n</b>"
